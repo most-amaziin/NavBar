1d3d30ac477a5b9d7759c7b05a3d1abc07ea1a42
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-12 14:22:49 -0500

    working on style still

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -62,7 +62,7 @@ export default class NavBar extends Component {
   render() {
     return(
       <div className='navBar'>
-        <h1>Yee-Haw Eh!</h1>
+        <h1 id='yeeHawEh'>Yee-Haw Eh!</h1>
         <div className="searchField">
           <input 
             className='search' autoComplete='off' 
