bc2ffa5ca5cefbc605ae6ce772c54d3367be15e4
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-13 08:07:12 -0500

    Added check on render

--- client/src/ShoppingCart.js
+++ client/src/ShoppingCart.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 const ShoppingCart = (props) => {
-  console.log(props.itemQty)
+    const inCart = {};
     return (
       <>
         <ul id='shopping-dropdown'>
           <li id='totalPrice' className='cartTotal'>Total: {props.total}</li>
-          {props.shoppingCart.map((item, i) => (
-            <li className='shoppingItem' id={`shoppingItem${i}`}>
-              <div className='shoppingItemName'>{item.name}</div>
-              <div className='shoppingItemPrice'>${item.price}</div>
-              <div className='shoppingItemQty'>x{item.qtyInCart}</div>
-              <div className='removeItem' onClick={() => props.handleRemove(i)}><i class="fas fa-minus-circle"></i></div>
-            </li>
-          ))}
+          {props.shoppingCart.map((item, i) => {
+            if(!inCart[item.name]) {
+              return (
+                <li className='shoppingItem' id={`shoppingItem${i}`}>
+                  <div className='shoppingItemName'>{item.name}</div>
+                  <div className='shoppingItemPrice'>${item.price}</div>
+                  <div className='shoppingItemQty'>x{item.qtyInCart}</div>
+                  <div className='removeItem' onClick={() => props.handleRemove(i)}><i class="fas fa-minus-circle"></i></div>
+                </li>
+              )
+            } else {
+              inCart[item.name] = true;
+            }
+          })}
         </ul>
       </>
     )
